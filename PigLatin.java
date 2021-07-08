d248b24fac14ad5c188d75056195b10d87f2d71c
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2021-07-08 18:04:01 +0300

    Simple Pig Latin kata solved

---
+++ PigLatin.java
@@ -0,0 +1,12 @@
+/*
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
+public class PigLatin {
+    public static String pigIt(String str) {
+        return str.replaceAll("(\\w)(\\w*)", "$2$1ay");
+    }
+}
