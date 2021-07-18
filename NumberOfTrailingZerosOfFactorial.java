9afb94989cdd602111d13fd9aa1342981fc84c87
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2021-07-18 20:23:46 +0300

    Updated solution of kata

--- NumberOfTrailingZerosOfFactorial.java
+++ NumberOfTrailingZerosOfFactorial.java
@@ -1,7 +1,7 @@
 public class Solution {
   public static int zeros(int n) {
     int count = 0;
-    for (int i = 5; n / i >= 1; i *= 5) {
+    for (int i = 5; i <= n; i *= 5) {
       count += n / i;
     }
     return count;
